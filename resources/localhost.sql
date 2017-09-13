3f2b976d965b63da5915389c74e109e9f6ca2596
erchoc <erchoc@qq.com>
2017-09-13 11:51:38 +0800

    fix db construct

--- resources/localhost.sql
+++ resources/localhost.sql
@@ -1,45 +1,13 @@
--- phpMyAdmin SQL Dump
--- version phpStudy 2014
--- http://www.phpmyadmin.net
---
--- 主机: localhost
--- 生成日期: 2017 年 09 月 12 日 18:07
--- 服务器版本: 5.5.53
--- PHP 版本: 5.4.45
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- 数据库: `shopping`
---
-
--- --------------------------------------------------------
-
---
--- 表的结构 `addresses`
---
-
-CREATE TABLE IF NOT EXISTS `addresses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `phone` varchar(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `admins`
---
+CREATE DATABASE `shopping` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `shopping`;
 
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -50,38 +18,45 @@ CREATE TABLE IF NOT EXISTS `admins` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
---
--- 转存表中的数据 `admins`
---
-
 INSERT INTO `admins` (`id`, `username`, `passwd`, `email`) VALUES
 (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `goods`
---
 
 CREATE TABLE IF NOT EXISTS `goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gname` varchar(255) NOT NULL,
+  `number` int(11) NOT NULL,
+  `gtype` varchar(255) NOT NULL,
+  `intro` varchar(255) NOT NULL,
+  `recommend` int(11) NOT NULL,
+  `integrals` int(11) NOT NULL,
+  `up_time` date NOT NULL,
   `old_price` float NOT NULL,
   `new_price` float NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `users`
---
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `gid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `order_time` date NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `remarks` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `passwd` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `sex` int(11) NOT NULL COMMENT '1男0女',
+  `phone` varchar(11) NOT NULL,
+  `register` date NOT NULL,
+  `grade` int(11) NOT NULL,
+  `integrals` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
