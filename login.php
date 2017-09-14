7e434e3740584e64d5865a29770d977ffe8a590f
Hennieishoney <honey_7294@163.com>
2017-09-14 16:03:55 +0800

    增加了用户注册登录的php

--- login.php
+++ login.php
@@ -5,15 +5,15 @@
   }//检测是否有submit操作 
  
   include('connect.php');//链接数据库
-  $name = $_POST['name'];//post获得用户名表单值
+  $name = $_POST['username'];//post获得用户名表单值
   $passowrd = $_POST['password'];//post获得用户密码单值
  
   if ($name && $passowrd){//如果用户名和密码都不为空
-       $sql = "select * from user where username = '$name' and password='$passowrd'";//检测数据库是否有对应的username和password的sql
+       $sql = "select * from users where username = '$name' and passwd='$passowrd'";//检测数据库是否有对应的username和password的sql
        $result = mysql_query($sql);//执行sql
        $rows=mysql_num_rows($result);//返回一个数值
        if($rows){//0 false 1 true
-          header("refresh:0;url=welcome.html");//如果成功跳转至welcome.html页面
+          header("refresh:0;url=index.html");//如果成功跳转至welcome.html页面
           exit;
        }else{
         echo "用户名或密码错误";
@@ -27,7 +27,7 @@
         
  
   }else{//如果用户名或密码有空
-        echo "用户名或密码不能为空";
+        echo "用户名密码不能为空";
         echo "
            <script>
               setTimeout(function(){window.location.href='login.html';},1000);
